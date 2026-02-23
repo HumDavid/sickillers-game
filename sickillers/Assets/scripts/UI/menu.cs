54a7c3a5592f2bd7c887f41f1ad2dc80822985d6
Guilherme Gaspar <guilhermevgaspar07@gmail.com>
2026-02-23 14:40:29 -0300

    Initializing scene of menu initial and configurantion personal enviroment

---
+++ sickillers/Assets/scripts/UI/menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+    void Start() {
+        
+    }
+    public void clickedStart() {
+        int life = 10
+    }
+    
+    void Update() {
+        
+    }
+}
